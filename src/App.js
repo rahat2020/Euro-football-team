4b9defc0f09d8e480cfa2b133373bed0a99d317c
rahat2020 <bloodyhell4040@gmail.com>
2021-03-11 20:08:14 +0600

    last try

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import Leagues from './components/Leagues/Leagues';
